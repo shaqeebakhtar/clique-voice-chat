9b33f97f67fe85049ef760078fcea3bd1674467f
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-02-05 13:41:22 +0530

    create space modal

---
+++ client/src/components/createSpaceModal/CreateSpaceModal.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./CreateSpaceModal.css";
+import { createSpace as create } from "../../utils/httpRequests";
+
+const CreateSpaceModal = ({ onClose }) => {
+  const [spaceType, setSpaceType] = useState("public");
+  const [topic, setTopic] = useState("");
+
+  const createSpace = async () => {
+    try {
+      if (!topic) return;
+      const { data } = await create({ topic, spaceType });
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="create-space__backdrop">
+      <div className="create-space__modal">
+        <div className="modal__header">
+          <h3 className="fw-black fs-title-sm">Create your Space</h3>
+          <button className="btn--close-modal" onClick={onClose}>
+            <img src="../../../public/assets/close.svg" />
+          </button>
+        </div>
+        <div className="modal__body">
+          <input
+            type="text"
+            name="topic"
+            id="topic"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+            placeholder="What do you want to talk about?"
+          />
+          <div className="space__types">
+            <div
+              className={
+                spaceType === "public" ? "space-type selected" : "space-type"
+              }
+              onClick={() => setSpaceType("public")}
+            >
+              <img src="../../../public/assets/globe.svg" alt="" />
+              <p className="fw-medium fs-body-x-sm">Public</p>
+            </div>
+            <div
+              className={
+                spaceType === "private" ? "space-type selected" : "space-type"
+              }
+              onClick={() => setSpaceType("private")}
+            >
+              <img src="../../../public/assets/lock.svg" alt="" />
+              <p className="fw-medium fs-body-x-sm">Private</p>
+            </div>
+            <div
+              className={
+                spaceType === "unlisted" ? "space-type selected" : "space-type"
+              }
+              onClick={() => setSpaceType("unlisted")}
+            >
+              <img src="../../../public/assets/link.svg" alt="" />
+              <p className="fw-medium fs-body-x-sm">Unlisted</p>
+            </div>
+          </div>
+        </div>
+        <div className="modal__footer">
+          <button onClick={createSpace} className="btn--start-space | fw-bold">
+            Start your Space
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateSpaceModal;
--- client/src/pages/spaces/Spaces.jsx
+++ client/src/pages/spaces/Spaces.jsx
@@ -1,42 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateSpaceModal from "../../components/createSpaceModal/CreateSpaceModal";
 import SpaceCard from "../../components/spaceCard/SpaceCard";
 import "./Spaces.css";
 
 const data = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
 
 const Spaces = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <section className="spaces | container">
-      <header className="spaces__header">
-        <div className="spaces__header-left">
-          <h3 className="fw-black fs-title-sm">Happening Now</h3>
-          <p className="fs-body-sm text-neutral-300">
-            Spaces live now, join and listen
-          </p>
-        </div>
-        <div className="spaces__header-right">
-          <div className="search-bar">
-            <label htmlFor="search">
-              <img src="../../../public/assets/search.svg" draggable="false" />
-            </label>
-            <input
-              type="text"
-              name="search"
-              placeholder="Search spaces"
-              id="search"
-            />
+    <>
+      <section className="spaces | container">
+        <header className="spaces__header">
+          <div className="spaces__header-left">
+            <h3 className="fw-black fs-title-sm">Happening Now</h3>
+            <p className="fs-body-sm text-neutral-300">
+              Spaces live now, join and listen
+            </p>
           </div>
-          <button className="btn--create-space | fw-bold">
-            Create your space
-          </button>
+          <div className="spaces__header-right">
+            <div className="search-bar">
+              <label htmlFor="search">
+                <img
+                  src="../../../public/assets/search.svg"
+                  draggable="false"
+                />
+              </label>
+              <input
+                type="text"
+                name="search"
+                placeholder="Search spaces"
+                id="search"
+              />
+            </div>
+            <button
+              className="btn--create-space | fw-bold"
+              onClick={() => setShowModal(true)}
+            >
+              Create your space
+            </button>
+          </div>
+        </header>
+        <div className="spaces-list">
+          {data.map((obj) => (
+            <SpaceCard key={obj.id} />
+          ))}
         </div>
-      </header>
-      <div className="spaces-list">
-        {data.map((obj) => (
-          <SpaceCard key={obj.id} />
-        ))}
-      </div>
-    </section>
+      </section>
+      {showModal && <CreateSpaceModal onClose={() => setShowModal(false)} />}
+    </>
   );
 };
 
